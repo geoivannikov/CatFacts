98d3f1ede2996fc527d44e5b136acaebeda21050
georgeivannikov <41365858+georgeivannikov@users.noreply.github.com>
2021-07-25 15:13:13 +0200

    FEAT: project inited

---
+++ CatFacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatFacts
+//
+//  Created by George Ivannikov on 25.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
